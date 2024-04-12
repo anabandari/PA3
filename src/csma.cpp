669bca9952d452ff317e0d5b29a2a07d73d9c955
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-04-11 21:59:52 -0700

    implementing csma calc!

--- src/csma.cpp
+++ src/csma.cpp
@@ -29,7 +29,7 @@ typedef struct{
 } node_info; 
 
 typedef struct{
-    int N, L, M, T; 
+    int N, L, M; 
     unsigned int total_time, current_time;
     vector<node_info> node;
     vector<int> R; // should we use a set or a vector...?
@@ -37,23 +37,66 @@ typedef struct{
 
 static values value;
 
-
-// Implement file input 
-void system_input(string filename){
-    ifstream input;
-	input.open(filename); 
-	if (!input) {
-        cerr << "Error: Unable to open file!\n";
-		exit(0);
-    }
-}
-
 // write a function to calculate a backoff?? 
     // its just the modulus but sureee
 int backoff(int nodeID, int ticks, int R){
     int backoff; 
     backoff = (nodeID+ticks)% R;  //backoff = mod(nodeID + ticks, R);
     return(backoff);
+}
+
+// Implement file input 
+void system_inputs(string filename){
+    ifstream input;
+	input.open(filename); 
+	if (!input) {
+        cerr << "Error: Unable to open file!\n";
+		exit(0);
+    }
+
+    string token;
+    while (input >> token) {
+        // Number of Nodes
+        if(token == "N"){
+            int value_N;
+            input >> value_N; // Read the value of N
+            value.N = value_N; // Store it in the values struct
+        }
+        else if(token == "L"){
+            int value_L;
+            input >> value_L; // Read the value of N
+            value.L = value_L; // Store it in the values struct
+        }
+        else if(token == "M"){
+            int value_M;
+            input >> value_M; // Read the value of N
+            value.L = value_M; // Store it in the values struct
+        }
+        // Random Seed Value
+        else if (token == "R") {
+            int value_R;
+            while (input >> value_R) {
+                value.R.push_back(value_R);
+            }
+        }
+        else if(token == "T"){
+            int value_T;
+            input >> value_T; // Read the value of N
+            value.total_time = value_T; // Store it in the values struct
+        }
+    }
+
+    input.close();
+}
+
+void instantialize_nodevec(){
+    for(int i; i < value.N; i++){
+        node_info node;
+        node.number = i; 
+        node.r_status = 0; 
+        node.backoff = backoff(node.number, 0, value.R[node.r_status]);
+        value.node.push_back(node);
+    }
 }
 
 
@@ -67,35 +110,41 @@ int backoff(int nodeID, int ticks, int R){
     if backoff_node_count != 1 
         for each node that is 0 assign random values for each of them. Increase random nodes based on the R vector tracker. 
 
-
 */
+
 int csma_calc(){
     vector<int> ready;
-    for(int i; i < value.N; i++){
-        if(value.node[i].backoff == 0){
-            ready.push_back(i); // would a set be better here????
+    int transmit_no_collide; 
+
+    while(value.current_time != value.total_time){
+        // Adding all nodes that have a backoff value of 0 to a vector
+        for(int i; i < value.N; i++){
+            if(value.node[i].backoff == 0){
+                ready.push_back(i); // would a set be better here????
+            }
         }
+
+        if(ready.size() > 1){
+            // collison occurs increase the R value!!
+        }
+
+        if(ready.size() == 1){
+            // no collison change the backoff for this node onlyyyy
+            // keep track of these number of slots!
+            // increase the total time by the link utililization or whateva
+
+        }
+
+        if(ready.size() == 0){
+            // clock ticks once more and all backoffs are decremented
+            for(int i; i < value.N; i++){
+                
+            }
+        }
+
     }
-
-    if(ready.size() > 1){
-        // collison occurs increase the R value!!
-    }
-
-    if(ready.size() == 1){
-        // no collison change the backoff for this node onlyyyy
-        // keep track of these number of slots!
-        // increase the total time by the link utililization or whateva
-
-    }
-
-    if(ready.size() == 0){
-        // clock ticks once more and all backoffs are decremented
-    }
-
     return(transmit_no_collide);
 }
-
-
 
 
 int main(int argc, char** argv){
@@ -103,6 +152,10 @@ int main(int argc, char** argv){
         printf("Wrong Input: ./csma input.txt\n");
         exit(0);
     }
+
+    // Sets up all the variables we need to run our simullaaaatioooonnn!!!!
+    system_inputs(argv[1]);
+    instantialize_nodevec();
 
     // Write the output of the simulator to a file called output.txt
 }
