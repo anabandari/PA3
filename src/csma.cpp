656f939fa39132ac4c93bec5bd94190e5cc5dfac
Ana Bandari <121533550+anabandari@users.noreply.github.com>
2024-04-11 18:27:05 -0700

    brainstorm dump

---
+++ src/csma.cpp
@@ -0,0 +1,79 @@
+/**  @file csma.cpp
+ *
+ *  @brief CSMA simulator
+ *
+ *  @author Ana Bandari (abandari)
+ *  @author Dajeong Kim (dkim2)
+ * 
+ *  @bug None known at this time.  
+ * 
+ */
+
+
+/*   Includes   */
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <map>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+typedef struct{
+ int N, L, M, R; 
+ unsigned int total_time, current_time; 
+
+} values;
+
+
+// File input
+void system_input(string filename){
+    ifstream input;
+	input.open(filename); 
+	if (!input) {
+        cerr << "Error: Unable to open file!\n";
+		exit(0);
+    }
+}
+
+// write a function to calculate a backoff??
+int backoff(int nodeID, int ticks, int R){
+    backoff = mod(nodeID + ticks, R);
+    return(backoff);
+}
+
+
+/*
+    Some pseudo-code:
+    if backoff == 0  
+        current time = current_clock_tick + L
+        Assign new random value 
+
+    Now is the issue of multilple nodes! 
+    if backoff_node_count != 1 
+        for each node that is 0 assign random values for each of them. Increase random nodes based on the R vector tracker. 
+
+
+*/
+void csma_calc(){
+    
+}
+
+
+
+
+int main(int argc, char** argv){
+    if(argc != 2){
+        printf("Wrong Input: ./csma input.txt\n");
+        exit(0);
+    }
+
+    // Write the output of the simulator to a file called output.txt
+}
+
+
