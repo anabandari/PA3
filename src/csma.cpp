d99d9658fa0674c8716c66814381072fe7f12eb1
kim-dajeong <kimdajeong10@gmail.com>
2024-04-15 17:53:28 -0700

    p

--- src/csma.cpp
+++ src/csma.cpp
@@ -11,38 +11,45 @@
 
 
 /*   Includes   */
+#include <string>    
+#include <vector>    
+#include <fstream>   
+#include <iostream> 
+#include <cstdlib>   
 #include <string.h>
+#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include <string.h>
 #include <algorithm>
 #include <map>
-#include <stdio.h>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
+/// Create struct for node information
 typedef struct{
-    int nodeID, r_status, backoff;
+    int nodeID, r_status;
+    double backoff;
 } node_info; 
 
+/// Create struct for nodes and parameters
 typedef struct{
     int N, L, M; 
     unsigned int total_time, current_time;
     vector<node_info> node;
-    vector<long> R; // should we use a set or a vector...?
+    vector<long> R; 
 } values;
 
 static values value;
 
-// write a function to calculate a backoff?? 
-    // its just the modulus but sureee
-int backoff(int nodeID, int ticks, int R){
-    int backoff; 
-    backoff = (nodeID+ticks)% R;  //backoff = mod(nodeID + ticks, R);
-    return(backoff);
+/// Function to calculate backoff
+double backoff(int nodeID, int ticks, int R){
+    double backoff; 
+    /// Backoff is equal to modulus of nodeID + ticks and backoff window
+    backoff = fmod((double)nodeID+(double)ticks,(double)R); 
+    /// Return calculated backoff value
+    return backoff;
 }
 
 // Implement file input 
@@ -50,26 +57,33 @@ void system_inputs(string filename){
     ifstream input;
 	input.open(filename); 
 	if (!input) {
+        /// Check that file is opened correctly
         cerr << "Error: Unable to open file!\n";
 		exit(0);
     }
 
+    /// Read input file and save values to parameters in struct "value"
     string token;
     while (getline(input, token)) {
         char* str = const_cast<char *>(token.c_str());
         long val;
+        /// Get value of number of nodes
         if (*str == 'N'){
             sscanf(str + 1, "%d", &value.N);
         }
+        /// Get value of packet size
         else if (*str == 'L'){
             sscanf(str + 1, "%d", &value.L);
         } 
+        /// Get value of maximum retransmission attempt
         else if (*str == 'M'){
             sscanf(str + 1, "%d", &value.M);
         }
+        /// Get value of total time of simulation (clock ticks)
         else if (*str == 'T'){
             sscanf(str + 1, "%u", &value.total_time);
         }
+        /// Get initial random number range
         else if (*str == 'R') {
             val = strtol(str + 1, &str, 10);
             while (val){
@@ -79,15 +93,19 @@ void system_inputs(string filename){
         }
     }
 
-
+    /// Close file after reading and obtaining all required parameters
     input.close();
 }
 
+/// Instantiate a vector of node_info structs
 void instantialize_nodevec(){
+    /// increment up to N number of nodes
     for(int i = 0; i < value.N; i++){
         node_info node;
+        /// set value of nnodeID to increment index
         node.nodeID = i;
         node.r_status = 0; 
+        /// Get value of backoff from backoff function
         node.backoff = backoff(node.nodeID, 0, value.R[node.r_status]);
         value.node.push_back(node);
     }
@@ -156,22 +174,26 @@ int csma_calc(){
 
 int main(int argc, char** argv){
     if(argc != 2){
-        printf("Wrong Input: ./csma input.txt\n");
+        /// Check for correct input file called "input"
+        printf("Wrong Input. Try: ./csma input\n");
         exit(0);
     }
 
     int num_successes;
     double calculation;
-    // Sets up all the variables we need to run our simullaaaatioooonnn!!!!
+    /// Sets up all the variables we need to run simulation from input file
     system_inputs(argv[1]);
+    /// Function to instantiate a vector of node_info structs
     instantialize_nodevec();
+    /// Get number of successes
     num_successes = csma_calc();
 
     calculation = ((double)num_successes)/((double)value.total_time);
 
-    // Write the output of the simulator to a file called output.txt
+    /// Write the output of the simulator to a file called output.txt
     ofstream outfile("output.txt");
-    outfile << calculation;
+    /// Round output to 2 decimal points
+    outfile << std::fixed << std::setprecision(2) << calculation;
     outfile.close(); 
  
 }
